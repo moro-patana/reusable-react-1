053c40b064251959a6f2d8be44fe985519b16445
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-24 15:43:12 +0300

    updated badge

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Badge = () => {
-	return <span>Hello, world!</span>;
+const Badge = ({children, color, ...props}) => {
+	return <span {...props} className={`badge ${color}`}>{children}</span>;
 };
 
 const App = () => {
 	return (
 		<section>
 			<h1>Check out these badges!</h1>
-			<Badge color="green">Success</Badge> This is operational. <br />
+			<Badge color="green" onClick={() => {
+				console.log("It clicked");
+			}}>Success</Badge> This is operational. <br />
 			<Badge color="red">Removed</Badge> This is critical. <br />
 			<Badge color="yellow">Warning</Badge> This is a warning. <br />
 			<Badge color="blue">Beta</Badge> This is in progress. <br />
